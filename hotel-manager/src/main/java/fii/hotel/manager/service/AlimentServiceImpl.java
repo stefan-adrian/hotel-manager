8fc6d37ce1365b5eb0e8c338ccd7e8d7e45ec984
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-02-21 13:42:05 +0200

    Added logging configuration

--- hotel-manager/src/main/java/fii/hotel/manager/service/AlimentServiceImpl.java
+++ hotel-manager/src/main/java/fii/hotel/manager/service/AlimentServiceImpl.java
@@ -2,12 +2,15 @@ package fii.hotel.manager.service;
 
 import fii.hotel.manager.model.Aliment;
 import fii.hotel.manager.repository.AlimentRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AlimentServiceImpl implements AlimentService {
     private AlimentRepository alimentRepository;
+    private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
     @Autowired
     public AlimentServiceImpl(AlimentRepository alimentRepository) {
@@ -17,6 +20,7 @@ public class AlimentServiceImpl implements AlimentService {
     @Override
     public Aliment save(Aliment aliment) {
         Aliment alimentSaved = alimentRepository.save(aliment);
+        logger.info("New aliment "+alimentSaved.getName()+" with id "+alimentSaved.getId()+" was saved in the database.");
         return alimentSaved;
     }
 }
