c060dbb858f42717ba803493171dd3758eb26c20
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-02-21 14:23:40 +0200

    Add swagger configuration

---
+++ hotel-manager/src/main/java/fii/hotel/manager/config/ApplicationConfig.java
@@ -0,0 +1,59 @@
+package fii.hotel.manager.config;
+
+import com.google.common.base.Predicates;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableSwagger2
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurationSupport {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(Predicates.or(
+                        RequestHandlerSelectors.basePackage("fii.hotel.manager.controller")))
+                .paths(Predicates.or(
+                        regex("/aliments.*")))
+                .build()
+                .apiInfo(metaData());
+    }
+
+    private ApiInfo metaData() {
+        return new ApiInfoBuilder()
+                .title("HOTEL MANAGER API")
+                .version("1.0.0")
+                .build();
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/api/v2/api-docs", "/v2/api-docs");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/ui", "/swagger-resources/configuration/ui");
+        registry.addRedirectViewController("/api/swagger-resources/configuration/security", "/swagger-resources/configuration/security");
+        registry.addRedirectViewController("/api/swagger-resources", "/swagger-resources");
+        registry.addRedirectViewController("/api", "/api/swagger-ui.html");
+    }
+
+    @Override
+    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+        registry.addResourceHandler("/api/**")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
--- hotel-manager/src/main/java/fii/hotel/manager/controller/AlimentController.java
+++ hotel-manager/src/main/java/fii/hotel/manager/controller/AlimentController.java
@@ -4,6 +4,9 @@ import fii.hotel.manager.dto.AlimentDto;
 import fii.hotel.manager.mapper.AlimentMapper;
 import fii.hotel.manager.model.Aliment;
 import fii.hotel.manager.service.AlimentService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/aliments")
+@Api(description = "Aliments requests")
 public class AlimentController {
     private AlimentService alimentService;
     private AlimentMapper alimentMapper;
@@ -22,6 +26,8 @@ public class AlimentController {
         this.alimentMapper = alimentMapper;
     }
 
+    @ApiOperation(value = "Add a new aliment")
+    @ApiResponse(code = 200, message = "Aliment successfully added")
     @PostMapping
     public AlimentDto add(@RequestBody AlimentDto alimentDto) {
         Aliment aliment = alimentMapper.map(alimentDto);
