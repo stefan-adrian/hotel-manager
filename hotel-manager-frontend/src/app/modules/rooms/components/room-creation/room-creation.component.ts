2153b2fc2138f45f7a4a6ca19844de9b859e1cc8
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-03-19 17:22:00 +0200

    Add RoomCreationComponent

---
+++ hotel-manager-frontend/src/app/modules/rooms/components/room-creation/room-creation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-room-creation',
+  templateUrl: './room-creation.component.html',
+  styleUrls: ['./room-creation.component.css']
+})
+export class RoomCreationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- hotel-manager-frontend/src/app/modules/rooms/rooms-routing.module.ts
+++ hotel-manager-frontend/src/app/modules/rooms/rooms-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {RoomListComponent} from "./components/room-list/room-list.component";
 import {RoomDetailComponent} from "./components/room-detail/room-detail.component";
+import {RoomCreationComponent} from "./components/room-creation/room-creation.component";
 
 const routes: Routes = [
   {
@@ -12,6 +13,10 @@ const routes: Routes = [
     path: ':id',
     component: RoomDetailComponent
   },
+  {
+    path: 'add',
+    component: RoomCreationComponent
+  }
 ];
 
 @NgModule({
