7de486c55bab0e0b6c92944f8490f7548ffb477a
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-03-19 23:10:09 +0200

    Create room form and request post

--- hotel-manager-frontend/src/app/shared/shared.module.ts
+++ hotel-manager-frontend/src/app/shared/shared.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HttpClientModule} from "@angular/common/http";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   exports: [
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+}
