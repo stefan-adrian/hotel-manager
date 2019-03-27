b99ef19eb09e996a9c90b427af1ecf9ef0b92749
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-03-27 20:50:09 +0200

    Delete unused code

--- hotel-manager-frontend/src/app/core/services/room.service.ts
+++ hotel-manager-frontend/src/app/core/services/room.service.ts
@@ -2,18 +2,14 @@ import {Injectable} from '@angular/core';
 import {ApiService} from "./api.service";
 import {Observable} from "rxjs";
 import {Room} from "../models/room.model";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
 
 @Injectable({
   providedIn: 'root'
 })
 export class RoomService {
 
-  private options = {headers: new HttpHeaders().set("Accept", "application/json" )};
-
   constructor(
-    private apiService: ApiService,
-    private httpClient: HttpClient
+    private apiService: ApiService
   ) {
   }
 
@@ -33,7 +29,5 @@ export class RoomService {
     let formData = new FormData();
     formData.append('image',uploadFile,uploadFile.name);
     this.apiService.patch(`/rooms/${room.id}/image`,formData).subscribe();
-    // this.httpClient
-    //   .patch("http://localhost:8082/rooms/"+room.id+"/image", formData, this.options).subscribe();
   }
 }
