492ea47e38c6462fcc8559deb3d9dcc17a5d15c6
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-03-28 16:18:39 +0200

    Aliment creation form

--- hotel-manager-frontend/src/app/modules/aliments/components/aliment-creation/aliment-creation.component.ts
+++ hotel-manager-frontend/src/app/modules/aliments/components/aliment-creation/aliment-creation.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {Message} from "primeng/api";
+import {AlimentService} from "../../../../core/services/aliment.service";
+import {Aliment} from "../../../../core/models/aliment.model";
 
 @Component({
   selector: 'app-aliment-creation',
@@ -7,9 +11,56 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlimentCreationComponent implements OnInit {
 
-  constructor() { }
+  alimentCreationForm: FormGroup;
+  uploadFile: any;
+  imageForm: any;
+  message: Message[] = [];
 
-  ngOnInit() {
+  constructor(
+    private formBuilder: FormBuilder,
+    private alimentService: AlimentService
+  ) {
   }
 
+  ngOnInit() {
+    this.alimentCreationForm = this.createFormGroup();
+  }
+
+  createFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      name: [null, Validators.required],
+      price: [null, Validators.required]
+    });
+  }
+
+  revertFormGroup() {
+    this.uploadFile = null;
+    this.alimentCreationForm = this.createFormGroup();
+    this.imageForm.clear();
+  }
+
+  save() {
+    const alimentToCreate: Aliment = Object.assign({},
+      this.alimentCreationForm.value);
+    console.log("AICI");
+    this.alimentService.add(alimentToCreate).subscribe(result=>
+    {
+      let aliment=JSON.parse(JSON.stringify(result));
+      this.alimentService.addImage(aliment,this.uploadFile);
+      this.revertFormGroup();
+    });
+  }
+
+
+  onUpload(event,form){
+    for(let file of event.files){
+      this.uploadFile=file;
+    }
+    this.imageForm=form;
+  }
+
+  showSuccess() {
+    this.message = [];
+    this.message.push({severity:'info', summary:'Aliment Created'});
+  }
 }
