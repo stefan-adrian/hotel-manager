029226cb01610145254fb4a647a1b0543e960d80
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-05-08 17:12:24 +0300

    Make footer stay at end of page

--- hotel-manager-frontend/src/app/app.component.ts
+++ hotel-manager-frontend/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'hotel-manager-frontend';
+  clientHeight: number;
+
+  constructor() {
+    this.clientHeight = window.innerHeight;
+  }
 }
