c7b4bdbb3c0a75a78fbde87151ed48f8fdd77de9
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-05-30 15:39:00 +0300

    Create categoriesPrices map in price service

--- hotel-manager/src/main/java/fii/hotel/manager/service/PriceServiceImpl.java
+++ hotel-manager/src/main/java/fii/hotel/manager/service/PriceServiceImpl.java
@@ -13,9 +13,17 @@ public class PriceServiceImpl implements  PriceService{
 
     private Map<String,Double> categoriesPrices=new HashMap<>();
 
+    public PriceServiceImpl() {
+    }
 
     @Override
     public Map<String, Double> getCategoriesPricesBetweenDates(List<CategoryBookingDto> categoryBookingDtos, LocalDate arrivalDate, LocalDate departureDate) {
+        categoryBookingDtos.forEach(categoryBookingDto -> categoriesPrices.put(categoryBookingDto.getName(),
+                                                            calculateCategoryPrice(categoryBookingDto,arrivalDate,departureDate)));
         return null;
+    }
+
+    private Double calculateCategoryPrice(CategoryBookingDto categoryBookingDto,LocalDate arrivalDate,LocalDate departureDate){
+        return 0.0;
     }
 }
