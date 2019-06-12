dacaad92bab03beb58912f961261920c22e49023
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-06-12 23:29:11 +0300

    Close order dialog modal after order

--- hotel-manager-frontend/src/app/modules/aliments/components/cart-dialog/cart-dialog.component.ts
+++ hotel-manager-frontend/src/app/modules/aliments/components/cart-dialog/cart-dialog.component.ts
@@ -1,6 +1,6 @@
 import {Component, Inject, OnInit} from '@angular/core';
 import {Aliment} from "../../../../core/models/aliment.model";
-import {MAT_DIALOG_DATA} from "@angular/material";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material";
 import {DialogData} from "../aliment-list/aliment-list.component";
 import {Booking} from "../../../../core/models/booking.model";
 import {BookingService} from "../../../../core/services/booking.service";
@@ -15,21 +15,23 @@ import {Router} from "@angular/router";
 })
 export class CartDialogComponent implements OnInit {
 
-  private aliments:Aliment[];
+  private aliments: Aliment[];
   private orderPrice: number;
-  private selectedBooking:Booking;
+  private selectedBooking: Booking;
   private bookings: Booking[];
 
   constructor(
     private bookingService: BookingService,
-    private roomserviceService:RoomserviceService,
+    private roomserviceService: RoomserviceService,
     private router: Router,
-  @Inject(MAT_DIALOG_DATA) public data: DialogData
-  ) { }
+    public dialogRef: MatDialogRef<CartDialogComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData
+  ) {
+  }
 
   ngOnInit() {
-    this.selectedBooking=null;
-    this.aliments=this.data.aliments;
+    this.selectedBooking = null;
+    this.aliments = this.data.aliments;
     this.calculateOrderPrice();
     this.getUserActiveBookings();
   }
@@ -41,17 +43,19 @@ export class CartDialogComponent implements OnInit {
     });
   }
 
-  private calculateOrderPrice():void{
-    this.orderPrice=0;
-    for(let aliment of this.aliments){
-      this.orderPrice+=aliment.price;
+  private calculateOrderPrice(): void {
+    this.orderPrice = 0;
+    for (let aliment of this.aliments) {
+      this.orderPrice += aliment.price;
     }
   }
-  private order(): void{
-    let roomserviceCreation=new RoomserviceCreation();
-    roomserviceCreation.aliments=this.aliments;
-    roomserviceCreation.bookingId=this.selectedBooking.id;
-    this.roomserviceService.add(roomserviceCreation).subscribe(result=>{
+
+  private order(): void {
+    let roomserviceCreation = new RoomserviceCreation();
+    roomserviceCreation.aliments = this.aliments;
+    roomserviceCreation.bookingId = this.selectedBooking.id;
+    this.roomserviceService.add(roomserviceCreation).subscribe(result => {
+      this.dialogRef.close();
       this.router.navigate(['/profile']);
     });
   }
