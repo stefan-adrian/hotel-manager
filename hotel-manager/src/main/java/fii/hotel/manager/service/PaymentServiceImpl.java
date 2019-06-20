fe363df0052eef321c3e52f9bb25a469ec420883
stefan-adrian <adrian.stefan@info.uaic.ro>
2019-06-20 22:55:36 +0300

    Add complete documentation with images

--- hotel-manager/src/main/java/fii/hotel/manager/service/PaymentServiceImpl.java
+++ hotel-manager/src/main/java/fii/hotel/manager/service/PaymentServiceImpl.java
@@ -13,65 +13,17 @@ import java.util.List;
 
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
     private final String RECEIVER_ACCOUNT_ID = "AShmTt5td4mjBj19DVDYZlH60ErzYlOhkaK1o92jWSq9XljyGC2spSnUkbPgnJ-30P8RHt_pDNmAkvsu";
     private final String RECEIVER_ACCOUNT_SECRET = "ED7j7RfByjIquagJd0priGVxqdUPwil6xPrIlXi0mxi9hx0ju5PjLTcPuEXEpWFPnP67T9TvrF09QTeN";
     private final String EXECUTION_MODE = "sandbox";
-
-    private Payment definePaymet(PaymentCreationDto paymentCreationDto) {
-        // Set payer details
-        Payer payer = new Payer();
-        payer.setPaymentMethod("paypal");
-
-        // Set redirect URLs
-        RedirectUrls redirectUrls = new RedirectUrls();
-        redirectUrls.setCancelUrl(Utils.REQUEST_SOURCE + "/home");
-        redirectUrls.setReturnUrl(Utils.REQUEST_SOURCE + "/profile/bookings");
-
-        // Set payment details
-        Details details = new Details();
-        details.setShipping("0");
-        details.setSubtotal(paymentCreationDto.getAmount().toString());
-        details.setTax("0");
-
-        // Payment amount
-        Amount amount = new Amount();
-        amount.setCurrency("USD");
-        // Total must be equal to sum of shipping, tax and subtotal.
-        amount.setTotal(paymentCreationDto.getAmount().toString());
-        amount.setDetails(details);
-
-
-        // Transaction information
-        Transaction transaction = new Transaction();
-        transaction.setAmount(amount);
-        transaction
-                .setDescription("Payment for " + paymentCreationDto.getItemName() + ".");
-
-        // Add transaction to a list
-        List<Transaction> transactions = new ArrayList<>();
-        transactions.add(transaction);
-
-        // Add payment details
-        Payment payment = new Payment();
-        payment.setIntent("sale");
-        payment.setPayer(payer);
-        payment.setRedirectUrls(redirectUrls);
-        payment.setTransactions(transactions);
-        return payment;
-    }
-
     @Override
     public String createPayment(PaymentCreationDto paymentCreationDto) {
         Payment payment = definePaymet(paymentCreationDto);
         APIContext apiContext = new APIContext(RECEIVER_ACCOUNT_ID, RECEIVER_ACCOUNT_SECRET, EXECUTION_MODE);
-
-        // Create payment
         try {
             Payment createdPayment = payment.create(apiContext);
-
-            Iterator links = createdPayment.getLinks().iterator();
-            while (links.hasNext()) {
-                Links link = (Links) links.next();
+            for (Links link : createdPayment.getLinks()) {
                 if (link.getRel().equalsIgnoreCase("approval_url")) {
                     return link.getHref();
                 }
@@ -80,7 +32,54 @@ public class PaymentServiceImpl implements PaymentService {
             System.err.println(e.getDetails());
         }
         return null;
+    }
 
+    private Payment definePaymet(PaymentCreationDto paymentCreationDto) {
+        Payment payment = new Payment();
+        payment.setIntent("sale");
+        payment.setPayer(createPayer());
+        payment.setRedirectUrls(createRedirectUrls());
+        payment.setTransactions(createPaymentTransactions(paymentCreationDto));
+        return payment;
+    }
+
+    private Payer createPayer(){
+        Payer payer = new Payer();
+        payer.setPaymentMethod("paypal");
+        return payer;
+    }
+
+    private RedirectUrls createRedirectUrls(){
+        RedirectUrls redirectUrls = new RedirectUrls();
+        redirectUrls.setCancelUrl(Utils.REQUEST_SOURCE + "/home");
+        redirectUrls.setReturnUrl(Utils.REQUEST_SOURCE + "/profile/bookings");
+        return  redirectUrls;
+    }
+
+    private Details createPaymentDetails(PaymentCreationDto paymentCreationDto){
+        Details details = new Details();
+        details.setShipping("0");
+        details.setSubtotal(paymentCreationDto.getAmount().toString());
+        details.setTax("0");
+        return details;
+    }
+
+    private Amount createPaymentAmount(PaymentCreationDto paymentCreationDto){
+        Amount amount = new Amount();
+        amount.setCurrency("USD");
+        amount.setTotal(paymentCreationDto.getAmount().toString());
+        amount.setDetails(createPaymentDetails(paymentCreationDto));
+        return amount;
+    }
+
+    private List<Transaction> createPaymentTransactions(PaymentCreationDto paymentCreationDto){
+        Transaction transaction = new Transaction();
+        transaction.setAmount(createPaymentAmount(paymentCreationDto));
+        transaction
+                .setDescription("Payment for " + paymentCreationDto.getItemName() + ".");
+        List<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction);
+        return  transactions;
     }
 
     @Override
@@ -92,9 +91,7 @@ public class PaymentServiceImpl implements PaymentService {
 
         PaymentExecution paymentExecution = new PaymentExecution();
         paymentExecution.setPayerId(payerId);
-        Payment createdPayment;
-
-        createdPayment = payment.execute(apiContext, paymentExecution);
+        payment.execute(apiContext, paymentExecution);
     }
 
 }
